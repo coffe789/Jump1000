2c22d25fcbd0a2b0dcebf8a7a44d544499f49171
Coffe <jonathan_davey@outlook.com>
2021-09-28 21:23:27 +0800

    Tidied up comments

--- Scene/Player/Player.gd
+++ Scene/Player/Player.gd
@@ -11,17 +11,13 @@ const JUMP_ACCELERATION = 600
 const MAX_FALL_SPEED = 600
 const UP_DIRECTION = Vector2(0,-1)
 const JUMP_BUFFER_DURATION = 0.2
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
 var isJumpBuffered = false # Needs to be global so that it persists with timer.
-
+# Sets buffer jump flag, and initiates buffer jump if conditions are set
 func doBufferJump() -> bool:
 	var justJumpBuffered = false
 	if isJumpBuffered && is_on_floor():
@@ -38,11 +34,10 @@ func doBufferJump() -> bool:
 # If you let go of jump, stop going up. Also handles buffered case.
 func check_if_finish_jump(justJumpBuffered) -> void:
 	if ((Input.is_action_just_released("jump") && acceleration.y < 0)) \
-	or (justJumpBuffered && !Input.is_action_pressed("jump")): #release jump when going up
+	or (justJumpBuffered && !Input.is_action_pressed("jump")):
 		acceleration.y /= 2;
 
-# Get movement inputs and move the player accordingly
-# If jump in midair, set isJumpBuffered flag
+# Get movement inputs and set acceleration accordingly
 func get_movement_input() -> void:
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
@@ -55,21 +50,22 @@ func get_movement_input() -> void:
 		pass
 	if (Input.is_action_just_pressed("jump") && is_on_floor()):
 		acceleration.y -= JUMP_ACCELERATION
-	# End jump when let go of jump button, or if performing minimum buffer jump
 
-
+# Decelerates the player accordingly
 func apply_drag() -> void:
 	if is_on_floor():
 		acceleration.x *= FLOOR_DRAG
 	else:
 		acceleration.x *= AIR_DRAG
 
+# Keeps velocity and acceleration within defined range
+# TODO: instead only clamp movement added directly from inputs
 func clamp_movement() -> void:
 	acceleration.y= clamp(acceleration.y, -INF,MAX_FALL_SPEED)
 	velocity.x = clamp(velocity.x, -MAX_X_SPEED,MAX_X_SPEED)
 	acceleration.x = clamp(acceleration.x, -MAX_X_SPEED,MAX_X_SPEED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Controls every aspect of player physics
 func _physics_process(delta) -> void:
 	if is_on_floor():
 		acceleration.y=0
@@ -80,6 +76,4 @@ func _physics_process(delta) -> void:
 	acceleration.y += GRAVITY
 	clamp_movement()
 	velocity += acceleration
-	
 	velocity = move_and_slide(velocity,UP_DIRECTION)
-
