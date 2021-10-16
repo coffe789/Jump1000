e2a5126f6227f9e46e43fd90c4e43786e9570fec
BraffZacklin <BraffZacklin@GitHub.com>
2021-10-16 17:19:19 +0800

    Add check so player can't continue wallslides past the bounds of the wall, also added coyote jump to the wall jump

--- Script/PlayerStates/wallsliding.gd
+++ Script/PlayerStates/wallsliding.gd
@@ -1,6 +1,7 @@
 extends PlayerState
 
 func do_state_logic(delta):
+	start_coyote_time()
 	do_gravity(delta, MAX_FALL_SPEED, GRAVITY/400)
 	#do_normal_x_movement(delta,AIR_DRAG)
 	Player.velocity = Player.move_and_slide(Player.velocity, UP_DIRECTION)
@@ -10,5 +11,7 @@ func check_for_new_state() -> String:
 		return "idle"
 	if (Input.is_action_just_pressed("jump") || Player.isJumpBuffered):
 		return "walljumping"
+	if not can_wall_jump() and not Player.canCoyoteJump:
+		return "falling"
 	else:
 		return "wallsliding"
