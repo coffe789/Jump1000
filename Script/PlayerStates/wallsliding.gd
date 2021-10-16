3bb949a881373f464dd5180d7863b812774a9904
BraffZacklin <BraffZacklin@GitHub.com>
2021-10-16 16:51:50 +0800

    Added slowed gravity to wallsliding state

--- Script/PlayerStates/wallsliding.gd
+++ Script/PlayerStates/wallsliding.gd
@@ -1,7 +1,7 @@
 extends PlayerState
 
 func do_state_logic(delta):
-	do_gravity(delta, MAX_FALL_SPEED, GRAVITY)
+	do_gravity(delta, MAX_FALL_SPEED, GRAVITY/400)
 	#do_normal_x_movement(delta,AIR_DRAG)
 	Player.velocity = Player.move_and_slide(Player.velocity, UP_DIRECTION)
 
