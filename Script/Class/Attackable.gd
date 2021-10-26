82814afea90460527611a18d6b7e2158e7726e36
coffe789 <thekingofcoffe@gmail.com>
2021-10-26 22:15:39 +0800

    Started working on the dashable area detection

--- Script/Class/Attackable.gd
+++ Script/Class/Attackable.gd
@@ -1,5 +1,33 @@
 extends Node2D
+class_name Attackable
+onready var Attackable_Area = get_node("AttackableArea") #All attackables must have an area called this
+
+signal attack_response
+var last_seen_attack_id = -1
+var Player
+
+#called by the player state
+func on_attacked(attack_damage, attack_type):
+	pass
 
 func _ready():
-	add_to_group("attackable")
-	add_user_signal("attack_response",["number"])
+	self.add_to_group("attackable")
+	Player = get_tree().get_nodes_in_group("player")[0]
+	Attackable_Area.connect("area_entered", self, "_on_AttackableArea_area_entered")
+
+# Tell the player what it just attacked
+func _on_AttackableArea_area_entered(area):
+	if area.is_in_group("player_attack") && Player.current_attack_id != last_seen_attack_id:
+		last_seen_attack_id = Player.current_attack_id
+		get_tree().call_group("player", "attack_response", Globals.NORMAL_STAGGER, self)
+
+func make_dashable():
+	self.remove_from_group("undashable")
+	self.add_to_group("dashable")
+	Attackable_Area.monitorable = false
+	Attackable_Area.monitorable = true # Ensures the dash area can detect us
+
+func make_undashable():
+	self.remove_from_group("dashable")
+	self.add_to_group("undashable")
+	get_tree().call_in_group("dash_area", "detect_undashable")
---
+++ Script/Player/DashCheckUp.gd
@@ -0,0 +1,6 @@
+extends Area2D
+onready var Player = get_parent()
+
+func _ready():
+	pass # Replace with function body.
+
