a975646b6497fbcce7e101d9f15651efaa7b8235
Coffe <jonathan_davey@outlook.com>
2021-12-07 15:27:53 +0800

    Made objects reset when you leave the room and such

--- Script/Class/LevelArea.gd
+++ Script/Class/LevelArea.gd
@@ -4,7 +4,7 @@ class_name LevelArea
 onready var Player = get_tree().get_nodes_in_group("player")[0]
 onready var Cam = Player.get_node("PlayerCamera")
 
-const FREEZE_TIME = 0.6
+const FREEZE_TIME = 0.6 # When room transitioning
 const UP_TRANSITION_BOOST = -230
 
 func _ready():
@@ -19,19 +19,19 @@ func initialise_rooms():
 		room.cutout_shapes()
 		for overlapping_body in room.get_overlapping_bodies():
 			if overlapping_body.is_in_group("room_boundary"):
-				var pos_dif = room.global_position - overlapping_body.global_position #clip operation requires polygons to be in same coordiante space
-				var transformed_cutout_shape = PoolVector2Array([Vector2(),Vector2(),Vector2(),Vector2()]) #this is how you create a size 4 array in gdscript
+				var transformed_cutout_shape = PoolVector2Array([Vector2(),Vector2(),Vector2(),Vector2()]) # this is how you create a size 4 array in gdscript
+				var pos_dif = room.global_position - overlapping_body.global_position # clip operation requires polygons to be in same coordiante space
 				for i in range(0,4):
 					transformed_cutout_shape[i] = room.cutout_shape[i] + pos_dif
 				var new_poly = Geometry.clip_polygons_2d(overlapping_body.get_node("CollisionPolygon2D").polygon, transformed_cutout_shape)
 				overlapping_body.get_node("CollisionPolygon2D").polygon = new_poly[0]
-				if new_poly.size() > 1: #If polygon is split into 2+ pieces, create new static bodies
+				if new_poly.size() > 1: # If polygon is split into 2+ pieces, create new static bodies
 					for i in range(1,new_poly.size()):
 						var bound = overlapping_body.get_parent().add_boundary(new_poly[i])
 						var diff = bound.global_position - overlapping_body.global_position
-						for j in range(0,bound.get_child(0).polygon.size()): #clip operation only gets new shape, not position, so we set it ourselves
+						for j in range(0,bound.get_child(0).polygon.size()): # clip operation only gets new shape, not position, so we set it ourselves
 							bound.get_child(0).polygon[j]-=diff
-	for node in get_tree().get_nodes_in_group("room_boundary"): #All bounds are disabled by default. We wait until now to disable them so clipping works
+	for node in get_tree().get_nodes_in_group("room_boundary"): # All bounds are disabled by default. We wait until now to disable them so clipping works
 		if node is StaticBody2D:
 			node.get_child(0).call_deferred("set_disabled",true)
 	if Player.current_room:
--- Script/RoomStuff/Room.gd
+++ Script/RoomStuff/Room.gd
@@ -6,25 +6,40 @@ var left_x
 var right_x
 var top_y
 var bottom_y
-var boundary_margin_x = 1 #(pixels)
+var margin_x = 1 #(pixels)
+var margin_y = 1
 var extra_space_above = 32
 var cutout_shape = [0,0,0,0]
 var cutout_shape_internal = [0,0,0,0]
 
+var resetable_scene
 
 func _ready():
 	add_to_group("room")
 	init_boundarys()
 	spawn_boundary_rectangle()
 	init_cutout_shape()
+	init_killbox()
+	
+	set_resetable_scene()
+	$ResetableNodes.queue_free()
+
+
+func set_resetable_scene():
+	resetable_scene = $ResetableNodes.create_scene()
 
 
 func enter_room():
 	enable_bounds(true)
+	$KillBox/CollisionShape2D.call_deferred("set_disabled",false)
+	var scene_instance = resetable_scene.instance()
+	call_deferred("add_child",scene_instance)
 
 
 func exit_room():
 	enable_bounds(false)
+	$KillBox/CollisionShape2D.call_deferred("set_disabled",true)
+	$ResetableNodes.queue_free()
 
 
 # Sets coordinates of boundarys for if needed later
@@ -36,19 +51,25 @@ func init_boundarys():
 	right_x = left_x + room_size.x
 	bottom_y = top_y + room_size.y
 
+const KILLBOX_HEIGHT = 10.0
+func init_killbox():
+	$KillBox.position.y = bottom_y + KILLBOX_HEIGHT / 2 + 3
+	$KillBox/CollisionShape2D.shape.extents.x = (right_x - left_x) / 2
+
 
 # Creates collision shape covering the whole room. Will be cutout later to serve as a boundary
 func spawn_boundary_rectangle():
 	var boundary_shape = $Boundary/CollisionPolygon2D
-	boundary_shape.polygon[0] = Vector2(left_x,top_y) + Vector2(-boundary_margin_x,-extra_space_above-1)
-	boundary_shape.polygon[1] = Vector2(right_x,top_y) + Vector2(boundary_margin_x,-extra_space_above-1)
-	boundary_shape.polygon[2] = Vector2(right_x,bottom_y) + Vector2(boundary_margin_x,0)
-	boundary_shape.polygon[3] = Vector2(left_x,bottom_y) + Vector2(-boundary_margin_x,0)
+	boundary_shape.polygon[0] = Vector2(left_x,top_y) + Vector2(-margin_x,-extra_space_above-margin_y)
+	boundary_shape.polygon[1] = Vector2(right_x,top_y) + Vector2(margin_x,-extra_space_above-margin_y)
+	boundary_shape.polygon[2] = Vector2(right_x,bottom_y) + Vector2(margin_x,0)
+	boundary_shape.polygon[3] = Vector2(left_x,bottom_y) + Vector2(-margin_x,0)
 
 
-# Sets cutout_shape to a PoolVector2Array with the same geometry as the room + upper margin
+# Sets external cutout_shape to a PoolVector2Array with the same geometry as the room
+# Sets internal cutout_shape to the same shape + upper margin
 func init_cutout_shape():
-	#external
+	# external
 	cutout_shape[0] = Vector2(left_x,top_y)
 	cutout_shape[1] = Vector2(right_x,top_y)
 	cutout_shape[2] = Vector2(right_x,bottom_y)
@@ -74,12 +95,16 @@ func enable_bounds(state:bool):
 	for child in get_children():
 		if child.is_in_group("room_boundary")  && child is StaticBody2D:
 			child.get_child(0).call_deferred("set_disabled",!state)
-			print(child.get_child(0))
 
 
 func cutout_shapes():
 	var new_poly = Geometry.clip_polygons_2d($Boundary/CollisionPolygon2D.polygon, cutout_shape_internal)
 	$Boundary/CollisionPolygon2D.polygon = new_poly[0]
-	if new_poly.size() > 1: #If polygon is split into 2+ pieces, create new static bodies
+	if new_poly.size() > 1: # If polygon is split into 2+ pieces, create new static bodies
 		for i in range(1,new_poly.size()):
 			add_boundary(new_poly[i])
+
+
+func _on_KillBox_body_entered(body):
+	if body.is_in_group("player"):
+		print("kill da player")
