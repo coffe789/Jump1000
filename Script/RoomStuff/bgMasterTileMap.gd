7ad8dca5692ec62e7ffd804d21e055550ba6d0e5
Coffe <jonathan_davey@outlook.com>
2021-12-19 22:47:30 +0800

    More tileset stuff + prototype lighting

---
+++ Script/RoomStuff/bgMasterTileMap.gd
@@ -0,0 +1,14 @@
+extends PaddedTileMap
+const TILE_SIZE = 12
+
+func _ready():
+	merge_tilesets()
+
+func merge_tilesets():
+	for tilemap in get_tree().get_nodes_in_group("bg_join_tile"):
+		for cell in tilemap.get_used_cells():
+			set_cellv(cell + tilemap.global_position / TILE_SIZE, tilemap.get_cellv(cell))
+		tilemap.queue_free()
+	
+	update_bitmask_region()
+	do_paddings()
