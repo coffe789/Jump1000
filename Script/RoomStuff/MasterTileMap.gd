06db7a2eff4ef407c5577079c2de4386e2aeb1b8
Coffe <jonathan_davey@outlook.com>
2021-12-24 21:09:26 +0800

    Integrated tilesets into rooms better. Added decal tiles

--- Script/RoomStuff/MasterTileMap.gd
+++ Script/RoomStuff/MasterTileMap.gd
@@ -1,11 +1,12 @@
 extends PaddedTileMap
 const TILE_SIZE = 12
+export var group_to_merge = "join_tile"
 
 func _ready():
 	merge_tilesets()
 
 func merge_tilesets():
-	for tilemap in get_tree().get_nodes_in_group("join_tile"):
+	for tilemap in get_tree().get_nodes_in_group(group_to_merge):
 		for cell in tilemap.get_used_cells():
 			set_cellv(cell + tilemap.global_position / TILE_SIZE, tilemap.get_cellv(cell))
 		tilemap.queue_free()
